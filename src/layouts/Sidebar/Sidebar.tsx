394c0d3ff730bb1845d667959e64ed55d2f9d172
vienpn <vienpn.91@gmail.com>
2019-03-29 11:03:18 +0700

    add new client and style when collapsed

--- src/layouts/Sidebar/Sidebar.tsx
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Menu, Icon, Layout, Avatar} from 'antd';
+import { Icon, Avatar} from 'antd';
 
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
-const { Sider } = Layout;
-import { ClientInfo, FullName, ClientItem, StatusList, StatusItem, DateItem, StatusTags, ClientSide } from './styled';
+import { SiderCollapsible, ClientInfo, FullName, ClientItem, StatusList,
+    StatusItem, DateItem, StatusTags, ClientSide, ButtonSideBar,
+  } from './styled';
 
 /* ClientItem
 *    ClientInfo
@@ -26,9 +25,57 @@ class Sidebar extends React.PureComponent {
       collapsed: !this.state.collapsed,
     });
   }
+  public ClientItemRender = () => {
+    return(
+      <ClientItem
+          key="sub1"
+          title = {
+            <ClientInfo>
+              <Avatar
+                size={46}
+                style={{ color: '#fff', backgroundColor: '#3B415C' }}>
+                JS
+              </Avatar>
+              <FullName>John Samual</FullName>
+            </ClientInfo>
+          }
+        >
+          <StatusList key="1">
+            <StatusItem>
+              <DateItem>15/03/2018</DateItem>
+              <StatusTags tagName="position">Position</StatusTags>
+            </StatusItem>
+          </StatusList>
+          <StatusList key="2">
+            <StatusItem>
+              <DateItem>23/02/2017</DateItem>
+              <StatusTags tagName="strategy">Strategy</StatusTags>
+            </StatusItem>
+          </StatusList>
+          <StatusList key="3">
+            <StatusItem>
+              <DateItem>23/02/2017</DateItem>
+              <StatusTags tagName="products">Products</StatusTags>
+            </StatusItem>
+          </StatusList>
+          <StatusList key="4">
+            <StatusItem>
+              <DateItem>23/02/2017</DateItem>
+              <StatusTags tagName="advice">Advice</StatusTags>
+            </StatusItem>
+          </StatusList>
+          <StatusList key="5">
+            <StatusItem>
+              <DateItem>23/02/2017</DateItem>
+              <StatusTags tagName="done">Done</StatusTags>
+            </StatusItem>
+          </StatusList>
+        </ClientItem>
+    );
+  }
   public render(): JSX.Element {
     return (
-      <Sider width={295} trigger={null} collapsible collapsed={this.state.collapsed}>
+      <SiderCollapsible width={295} trigger={null} collapsible collapsed={this.state.collapsed}>
         <Icon
           className="trigger IconSider"
           type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
@@ -39,52 +86,18 @@ class Sidebar extends React.PureComponent {
           defaultOpenKeys={['sub1']}
           mode="inline"
         >
-          <ClientItem
-            key="sub1"
-            title = {
-              <ClientInfo>
-                <Avatar
-                  size={46}
-                  style={{ color: '#fff', backgroundColor: '#3B415C' }}>
-                  JS
-                </Avatar>
-                <FullName>John Samual</FullName>
-              </ClientInfo>
-            }
-          >
-            <StatusList key="1">
-              <StatusItem>
-                <DateItem>15/03/2018</DateItem>
-                <StatusTags tagName="position">Position</StatusTags>
-              </StatusItem>
-            </StatusList>
-            <StatusList key="2">
-              <StatusItem>
-                <DateItem>23/02/2017</DateItem>
-                <StatusTags tagName="strategy">Strategy</StatusTags>
-              </StatusItem>
-            </StatusList>
-            <StatusList key="3">
-              <StatusItem>
-                <DateItem>23/02/2017</DateItem>
-                <StatusTags tagName="products">Products</StatusTags>
-              </StatusItem>
-            </StatusList>
-            <StatusList key="4">
-              <StatusItem>
-                <DateItem>23/02/2017</DateItem>
-                <StatusTags tagName="advice">Advice</StatusTags>
-              </StatusItem>
-            </StatusList>
-            <StatusList key="5">
-              <StatusItem>
-                <DateItem>23/02/2017</DateItem>
-                <StatusTags tagName="done">Done</StatusTags>
-              </StatusItem>
-            </StatusList>
-          </ClientItem>
+          { this.ClientItemRender() }
+          { this.ClientItemRender() }
+          { this.ClientItemRender() }
+          { this.ClientItemRender() }
+          { this.ClientItemRender() }
+          { this.ClientItemRender() }
         </ClientSide>
-      </Sider>
+        <ButtonSideBar size="large" type="primary">
+          <Icon type="plus" />
+          <span>New Client</span>
+        </ButtonSideBar>
+      </SiderCollapsible>
     );
   }
 }
