ae2c76df7b8788938227cd56ced6de2ceb21ca68
vienpn <vien.phamngoc@gmail.com>
2019-10-02 16:53:55 +0700

    add data sample on step 2

--- src/components/DocumentsPage/DocumentsCarousel/DocumentsCarousel.tsx
+++ src/components/DocumentsPage/DocumentsCarousel/DocumentsCarousel.tsx
@@ -49,6 +49,46 @@ class DocumentsCarousel extends React.PureComponent<DocumentsCarouselProps> {
       key: 'reason',
     },
   ];
+  public dataSource2 = [
+    {
+      key: '1',
+      insurance: 'Insurance Nominations 2 ',
+      reason:
+        'You have informed us that your estate planning arrangements are in place and you do not require it to be re-addressed',
+    },
+    {
+      key: '2',
+      insurance: 'Sample text 2',
+      reason: 'Unspecified',
+    },
+    {
+      key: '3',
+      insurance: 'POA / EPOA 2',
+      reason: 'Unspecified',
+    },
+    {
+      key: '4',
+      insurance: 'Guardianship 2',
+      reason: 'Unspecified',
+    },
+    {
+      key: '5',
+      insurance: 'Enter Description',
+      reason: '-',
+    },
+  ];
+  public columns2 = [
+    {
+      title: 'Insurance',
+      dataIndex: 'insurance',
+      key: 'insurance',
+    },
+    {
+      title: 'Reason for being scoped out',
+      dataIndex: 'reason',
+      key: 'reason',
+    },
+  ];
   public render(): JSX.Element {
     return (
       <CarouselWrapper>
@@ -64,8 +104,8 @@ class DocumentsCarousel extends React.PureComponent<DocumentsCarouselProps> {
           <CarouselItem>
             <Table
               className={`table-general documents-table`}
-              columns={this.columns}
-              dataSource={this.dataSource}
+              columns={this.columns2}
+              dataSource={this.dataSource2}
               pagination={false}
             />
           </CarouselItem>
@@ -79,9 +119,9 @@ class DocumentsCarousel extends React.PureComponent<DocumentsCarouselProps> {
           </CarouselItem>
           <CarouselItem>
             <Table
-              className={cn('table-general documents-table`')}
-              columns={this.columns}
-              dataSource={this.dataSource}
+              className={`table-general documents-table`}
+              columns={this.columns2}
+              dataSource={this.dataSource2}
               pagination={false}
             />
           </CarouselItem>
