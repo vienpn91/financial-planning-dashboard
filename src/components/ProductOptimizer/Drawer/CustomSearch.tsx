cd8ee06257fdb391577032c81223f380e1535089
Khoa Nguyen <meo.wizard@gmail.com>
2019-10-22 11:06:13 +0700

    Remove log

--- src/components/ProductOptimizer/Drawer/CustomSearch.tsx
+++ src/components/ProductOptimizer/Drawer/CustomSearch.tsx
@@ -192,7 +192,6 @@ const CustomSearch = (props: CustomSearchProp) => {
 
     // Fund options for the specific
     if (type === CustomSearchType.Fund) {
-      console.log({ productId, type });
       switch (productId) {
         // CFS FirstChoice Wholesale Personal Super
         case 1:
