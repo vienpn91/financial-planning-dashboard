96c124283d3c9e77fd01375a38ef6ba1f2d0b7bb
Khoa Nguyen <meo.wizard@gmail.com>
2019-10-30 10:17:51 +0700

    Provide Retirement formatter

--- src/components/PresentationPage/PresentationStep3/SlidersBlock.tsx
+++ src/components/PresentationPage/PresentationStep3/SlidersBlock.tsx
@@ -10,6 +10,10 @@ function currencyFormatter(value: SliderValue) {
   return numeral(value).format('$ 0,0.[00]');
 }
 
+function retirementFormatter(value: SliderValue) {
+  return value + ' years';
+}
+
 const riskProfiles = ['Preservation', 'Defensive', 'Moderate', 'Balanced', 'Growth', 'High growth'];
 
 const convertArrayStringToMarks = (
@@ -36,7 +40,7 @@ const formatterFromArrayString = (array: string[]) => (value: SliderValue) => {
 const SlidersBlock = () => {
   return (
     <SlidersBlockWrapper>
-      <MySlider title={'Retirement'} min={57} max={100} defaultValue={65} step={1} />
+      <MySlider title={'Retirement'} min={57} max={100} defaultValue={65} step={1} formatter={retirementFormatter} />
       <MySlider title={'Income'} min={0} max={500000} defaultValue={100000} formatter={currencyFormatter} step={1000} />
       <MySlider
         title={'Expenses'}
