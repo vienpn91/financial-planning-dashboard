d130918ae56b7cff9b4f723d9e2cde2c5231d2df
Long Hoang <longhp@zigvy.com>
2019-11-06 15:23:04 +0700

    Render chart data when salary satisfy unchecked

--- src/components/PresentationPage/PresentationStep5/PresentationStep5.tsx
+++ src/components/PresentationPage/PresentationStep5/PresentationStep5.tsx
@@ -12,21 +12,70 @@ import EventsBlock from './EventsBlock';
 import ChartsBlock from './ChartsBlock';
 import { CheckboxGroup } from './styled';
 import { CheckboxCustomize } from '../../StrategyPage/StrategyTable/styled';
-import { chartsDataResources, chartsDataResourcesWithLifeEvent } from './chartData';
+import {
+  chartsDataResources,
+  chartsDataResourcesWithLifeEvent,
+  chartsDataResourcesWithoutSalarySacrifice,
+} from './chartData';
 
-const CheckboxContainer = (props: { children: React.ReactNode }) => {
+interface CheckListInterface {
+  'Salary Sacrifice'?: boolean;
+  'Non-Concessional Contribution'?: boolean;
+  'Establish a pension'?: boolean;
+  'Debt reduction'?: boolean;
+  Insurance?: boolean;
+  'Estate Planning'?: boolean;
+}
+
+const defaultCheckListValue = {
+  'Salary Sacrifice': true,
+  'Non-Concessional Contribution': true,
+  'Establish a pension': true,
+  'Debt reduction': true,
+  Insurance: true,
+  'Estate Planning': true,
+};
+
+const checkBoxList = [
+  'Salary Sacrifice',
+  'Non-Concessional Contribution',
+  'Establish a pension',
+  'Debt reduction',
+  'Insurance',
+  'Estate Planning',
+];
+
+const emptyFn = () => null;
+
+const CheckboxContainer = (props: { children: React.ReactNode; handleItemToggle?: (isToggle: boolean) => void }) => {
+  const { handleItemToggle = emptyFn, children } = props;
   const [value, setValue] = useState<boolean>(true);
   const onChange = (e: CheckboxChangeEvent) => {
     setValue(e.target.checked);
+    handleItemToggle(e.target.checked);
   };
 
   return (
     <CheckboxCustomize>
       <Checkbox checked={value} onChange={onChange}>
-        {props.children}
+        {children}
       </Checkbox>
     </CheckboxCustomize>
   );
+};
+
+const getData = (hasLifeEvent: boolean, retirementYrs: number, checkList?: CheckListInterface) => {
+  if (!(checkList as CheckListInterface)['Salary Sacrifice']) {
+    return (chartsDataResourcesWithoutSalarySacrifice as any)[retirementYrs];
+  }
+
+  if (hasLifeEvent) {
+    // return (chartsDataResourcesWithLifeEvent as any)[retirementYrs];
+    return (chartsDataResources as any)[retirementYrs];
+
+  }
+
+  return (chartsDataResources as any)[retirementYrs];
 };
 
 // forget about DRY
@@ -36,44 +85,46 @@ const PresentationStep5 = (props: FormikPartProps) => {
   // I have to do this because indra requires the task have to be completed within this afternoon
   // TODO: refactor this to reducer
   const [hasLifeEvent, setHasLifeEvent] = useState(false);
+  const [checkList, setCheckList] = useState(defaultCheckListValue);
   const [retirementYrs, setRetirementYrs] = useState(60);
   const [chartsData, setChartsData] = useState(chartsDataResources['60']);
+  // handle when a check box is checked or unchecked
+  const onSetCheckList = (itemName: string) => (isToggle: boolean) => {
+    const newCheckList = {
+      ...checkList,
+      [itemName]: isToggle,
+    };
+    const chartData = getData(hasLifeEvent, retirementYrs, newCheckList);
+    setCheckList(newCheckList);
+    setChartsData(chartData);
+  };
+  // handle the case that when a user added a life event
   const onSetHasLifeEvent = (hle: boolean) => {
-    if (hle) {
-      setChartsData((chartsDataResourcesWithLifeEvent as any)[retirementYrs]);
-    } else {
-      setChartsData((chartsDataResources as any)[retirementYrs]);
-    }
+    const chartData = getData(hle, retirementYrs, checkList);
+    setChartsData(chartData);
     setHasLifeEvent(hle);
   };
+  // handle retirement year has been changed
   const onChangeRetirementYear = (retirementYear: SliderValue) => {
-    if (retirementYear === 65) {
-      setRetirementYrs(65);
-      if (hasLifeEvent) {
-        setChartsData(chartsDataResourcesWithLifeEvent['65'] as any);
-      } else {
-        setChartsData(chartsDataResources['65']);
-      }
-    }
-    if (retirementYear === 60) {
-      setRetirementYrs(60);
-      if (hasLifeEvent) {
-        setChartsData(chartsDataResourcesWithLifeEvent['60'] as any);
-      } else {
-        setChartsData(chartsDataResources['60']);
-      }
-    }
+    return;
+    // const chartData = getData(hasLifeEvent, retirementYear as number, checkList);
+    // setChartsData(chartData);
+    // if (retirementYear === 65) {
+    //   setRetirementYrs(65);
+    // }
+    // if (retirementYear === 60) {
+    //   setRetirementYrs(60);
+    // }
   };
 
   return (
     <StepWrapper>
       <CheckboxGroup>
-        <CheckboxContainer>Salary Sacrifice</CheckboxContainer>
-        <CheckboxContainer>Non-Concessional Contribution</CheckboxContainer>
-        <CheckboxContainer>Establish a pension</CheckboxContainer>
-        <CheckboxContainer>Debt reduction</CheckboxContainer>
-        <CheckboxContainer>Insurance</CheckboxContainer>
-        <CheckboxContainer>Estate Planning</CheckboxContainer>
+        {checkBoxList.map((cb: string) => (
+          <CheckboxContainer key={cb} handleItemToggle={onSetCheckList(cb)}>
+            {cb}
+          </CheckboxContainer>
+        ))}
       </CheckboxGroup>
       <StepCurrentPosition>
         <StepPositionLeft style={{ flex: '0 0 295px', padding: '15px', border: '1px solid #dedede' }}>
@@ -81,7 +132,12 @@ const PresentationStep5 = (props: FormikPartProps) => {
           <EventsBlock setHasLifeEvent={onSetHasLifeEvent} />
         </StepPositionLeft>
         <StepPositionRight>
-          <ChartsBlock chartsData={chartsData} retirementYear={retirementYrs} hasLifeEvent={hasLifeEvent} />
+          <ChartsBlock
+            chartsData={chartsData}
+            retirementYear={retirementYrs}
+            hasLifeEvent={hasLifeEvent}
+            checkList={checkList}
+          />
         </StepPositionRight>
       </StepCurrentPosition>
     </StepWrapper>
