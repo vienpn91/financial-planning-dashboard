38f38dd7909a2f99b0cf62c65b1a6e3e13f5e6db
Long Hoang <44691721+longhpzigvy@users.noreply.github.com>
2019-11-09 12:59:11 +0700

    Allow user to toggle life event (#116)

--- src/components/PresentationPage/PresentationStep5/EventItem.tsx
+++ src/components/PresentationPage/PresentationStep5/EventItem.tsx
@@ -8,21 +8,29 @@ export interface Event {
   key: string;
   title: string;
   year: number;
+  checked: boolean;
 }
 
 interface EventItemProps {
   event: Event;
   index: number;
   onRemove: (index: number) => void;
+  handleOnCheck?: (checked: boolean) => void;
 }
 
+const emptyFn = () => null;
+
 const EventItem = (props: EventItemProps) => {
-  const { event, index, onRemove } = props;
+  const { event, index, onRemove, handleOnCheck = emptyFn } = props;
   const [checkboxValue, setCheckboxValue] = useState<boolean>(true);
+  const onItemChecked = (v: any) => {
+    setCheckboxValue(v);
+    handleOnCheck(v);
+  };
 
   return (
     <EventItemWrapper>
-      <CheckboxInput value={checkboxValue} onChange={setCheckboxValue} />
+      <CheckboxInput value={checkboxValue} onChange={onItemChecked} />
       <Sentence>
         <Owner>{event.key}</Owner> {event.title} added for the year <b>{event.year}</b>
       </Sentence>
